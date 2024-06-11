9cc03714150a75ff8850b6768f37fee38a3bdc85
Amee <ameecova@yahoo.com>
2024-06-11 23:14:57 +0200

    Create bare bones of pages and import bulma

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
-// src/App.jsx
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
 import Gallery from "./pages/Gallery";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/menu" element={<Menu />} />
@@ -16,6 +18,7 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/about" element={<About />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles.css";
+
+const Home = () => {
+  return <div className="home-background"></div>;
+};
+
+export default Home;
